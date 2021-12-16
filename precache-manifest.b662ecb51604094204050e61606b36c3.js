508621f627f059f594e3ade4de1e8d1d9de34497
Kenechukwu <kenechukwu.ajufo@gmail.com>
2021-12-16 01:43:17 +0000

    Updates

---
+++ precache-manifest.b662ecb51604094204050e61606b36c3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7874a8bdced695b693bdaf259a178803",
+    "url": "/ReactTodoApp/index.html"
+  },
+  {
+    "revision": "b38c9497904389107d8c",
+    "url": "/ReactTodoApp/static/js/2.50b0434f.chunk.js"
+  },
+  {
+    "revision": "2aa343dd4a815c220fe7",
+    "url": "/ReactTodoApp/static/js/main.2b62dbfe.chunk.js"
+  },
+  {
+    "revision": "aab0481c0946ff954065",
+    "url": "/ReactTodoApp/static/js/runtime-main.e5d5c467.js"
+  }
+]);
